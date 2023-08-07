0c84018473e959dfc51a902d470953619d6b0144
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-07 09:31:24 -0300

    home terminada

--- REACT/geriatrico-frontend/src/Fecha/Fecha.js
+++ REACT/geriatrico-frontend/src/Fecha/Fecha.js
@@ -3,31 +3,32 @@ import "./Fecha.css";
 
 function Fecha({ fechasRelevantes }) {
   if (Array.isArray(fechasRelevantes)) {
-    return fechasRelevantes.map((fr) => {
-      const fecha = new Date(fr.Fecha);
-      const dia = fecha.getUTCDate();
+    return (
+      <div className="fecha-container">
+        {fechasRelevantes.map((fr) => {
+          const fecha = new Date(fr.Fecha);
+          const dia = fecha.getUTCDate();
 
-      // Obtener la hora a partir de la cadena en formato "HH:mm:ss"
-      const horaString = fr.Hora; // Supongamos que fr.Hora es una cadena como "2023-07-31T14:30:00"
-      const hora = new Date(horaString).toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
+          let horaString = fr.Hora; 
+          let hora = new Date(horaString).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
 
-      if (hora === "00:00") {
-        hora = "Todo el dia";
-      }
+          if (hora === "00:00") {
+            hora = "Todo el dia";
+          }
 
-      return (
-        <div className="fecha-cuadrado" key={fr.id}>
-          <p>{dia}</p>
-          <p>{fr.Texto}</p>
-          <p>{hora}</p>
-          {/* Aplicar estilos para cambiar el tamaño de la imagen */}
-          <img src={fr.Imagen} alt="Imagen" style={{ height: "20px", width: "20px" }} />
-        </div>
-      );
-    });
+          return (
+            <div className="fecha-cuadrado" key={fr.id}>
+              <p className="fecha-dia">{dia}</p>
+              <p className="fecha-texto">{fr.Texto}</p>
+              <p className="fecha-hora">{hora}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
--- REACT/geriatrico-frontend/src/Home/Home.js
+++ REACT/geriatrico-frontend/src/Home/Home.js
@@ -6,34 +6,31 @@ import logo from "../images/logo.png";
 import menu from "../images/comida.jpg";
 
 function Home({ fechasRelevantes }) {
-  // Dividir el arreglo de fechas en dos partes
   const mitad = Math.ceil(fechasRelevantes.length / 2);
   const primeraMitad = fechasRelevantes.slice(0, mitad);
   const segundaMitad = fechasRelevantes.slice(mitad);
 
   return (
     <div className="home-container">
-      {/* Centrar el logo en la parte superior */}
       <img alt="" src={logo} className="logo" />
 
-      
-        <span className="Titulo-Fechas-relevantes"> Julio</span>
-        <Row>
-          <Col>
-            <Fecha fechasRelevantes={primeraMitad} />
-          </Col>
-          <Col>
-            <Fecha fechasRelevantes={segundaMitad} />
-          </Col>
-        </Row>
+      <span className="Titulo-Fechas-relevantes"> Julio</span>
+      <Row>
+        <Col>
+          <Fecha fechasRelevantes={primeraMitad} />
+        </Col>
+        <Col>
+          <Fecha fechasRelevantes={segundaMitad} />
+        </Col>
+      </Row>
 
-      {/* Rectángulo en la parte inferior */}
       <div className="menu-semanal-rectangulo">
-        <img alt="" src={menu} className="menu"/>
-        <h2>Menu Semanal</h2>
-        <p>
-          El menu de esta semana es muy variado, con muchas opciones para elegir.
-        </p>
+        <div className="menu">
+          <img alt="" src={menu} />
+        </div>
+        <div className="menu-text">
+          <p>Menu Semanal</p>
+        </div>
       </div>
     </div>
   );
