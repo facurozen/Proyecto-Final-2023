d399d00551ca97fb6cf36ddd20d1fbc5dfcd48de
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-15 09:53:48 -0300

    hecho scroll de fechas

--- REACT/geriatrico-frontend/src/Home/Home.js
+++ REACT/geriatrico-frontend/src/Home/Home.js
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Fecha from "../Fecha/Fecha";
 import logo from "../images/logo.png";
 import menu from "../images/comida.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Home({ fechasRelevantes }) {
-  const mitad = Math.ceil(fechasRelevantes.length / 2);
-  const primeraMitad = fechasRelevantes.slice(0, mitad);
-  const segundaMitad = fechasRelevantes.slice(mitad);
+  const [currentPage, setCurrentPage] = useState(0); // Página actual
+
+  // Número de fechas a mostrar en cada página
+  const itemsPerPage = 4;
+
+  // Calcular el índice de inicio y fin para la página actual
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Obtener las fechas relevantes para la página actual
+  const visibleFechasRelevantes = fechasRelevantes.slice(startIndex, endIndex);
+
+  // Función para ir a la página siguiente
+  const nextPage = () => {
+    if (startIndex + itemsPerPage < fechasRelevantes.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Función para ir a la página anterior
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <div className="home-container">
       <img alt="" src={logo} className="logo" />
 
-      <span className="Titulo-Fechas-relevantes"> Julio</span>
       <Row>
         <Col>
-          <Fecha fechasRelevantes={primeraMitad} />
-        </Col>
-        <Col>
-          <Fecha fechasRelevantes={segundaMitad} />
+          <Fecha fechasRelevantes={visibleFechasRelevantes} />
         </Col>
       </Row>
+
+      <div className="pagination-buttons">
+        <button onClick={prevPage} disabled={currentPage === 0}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button
+          onClick={nextPage}
+          disabled={startIndex + itemsPerPage >= fechasRelevantes.length}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
 
       <div className="menu-semanal-rectangulo">
         <div className="menu">
