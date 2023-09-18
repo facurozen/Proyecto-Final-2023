887215af11d38cb1d94f7de674d24e37ffdc725c
nicorozen <85349868+nicorozen@users.noreply.github.com>
2023-09-17 21:56:12 -0300

    terminando parte de perfil

--- REACT/geriatrico-frontend/src/BotonMedicamentos/BotonMediamentos.js
+++ REACT/geriatrico-frontend/src/BotonMedicamentos/BotonMediamentos.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './BotonMedicamentos.css';
 import Modal from 'react-bootstrap/Modal';
 import Table from 'react-bootstrap/Table';
@@ -7,11 +7,30 @@ import icono from './pills.svg';
 
 function BotonMedicamentos({ medicamentoATomar }) {
   const [show, setShow] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const modalRef = useRef(null);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleModalEntered = () => {
+    if (modalRef.current) {
+      modalRef.current.scrollTop = modalRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    if (modalRef.current) {
+      modalRef.current.scrollTop = scrollPosition;
+    }
+  }, [scrollPosition]);
+
+
+
   const renderMedicamentos = () => {
+    let currentDate = null;
+
     return (
       <tbody>
         {medicamentoATomar.map((med) => {
@@ -21,29 +40,51 @@ function BotonMedicamentos({ medicamentoATomar }) {
             minute: '2-digit',
           });
 
+          const formattedDate = fecha.toLocaleDateString();
+          const showDateHeader = currentDate !== formattedDate;
+
+          if (showDateHeader) {
+            currentDate = formattedDate;
+          }
+
           return (
-            <tr key={med.id}>
-              <td className="hora-cell">{hora}</td>
-              <td className="medicamento-cell">{med.NombreMedicamento}</td>
-              <td>
-                <Form>
-                  {['radio'].map((type) => (
-                    <div key={`default-${type}`} className="mb-3">
-                      <Form.Check
-                        type={type}
-                        id={`default-${type}`}
-                        checked={med.TomoMedicacion}
-                        label=""
-                      />
-                    </div>
-                  ))}
-                </Form>
-              </td>
-            </tr>
+            <React.Fragment key={med.id}>
+              {showDateHeader && (
+                <tr>
+                  <th colSpan="3" className="date-header">
+                    {formattedDate}
+                  </th>
+                </tr>
+              )}
+              <tr>
+                <td className="hora-cell">{hora}</td>
+                <td className="medicamento-cell">{med.NombreMedicamento}</td>
+                <td>
+                  <Form>
+                    {['radio'].map((type) => (
+                      <div key={`default-${type}`} className="mb-3">
+                        <Form.Check
+                          type={type}
+                          id={`default-${type}`}
+                          checked={med.TomoMedicacion}
+                          label=""
+                        />
+                      </div>
+                    ))}
+                  </Form>
+                </td>
+              </tr>
+            </React.Fragment>
           );
         })}
       </tbody>
     );
+  };
+
+  const handleModalScroll = () => {
+    if (modalRef.current) {
+      setScrollPosition(modalRef.current.scrollTop);
+    }
   };
 
   return (
@@ -52,11 +93,19 @@ function BotonMedicamentos({ medicamentoATomar }) {
         <img className="icon" src={icono} alt="icono" />
         <span className="-sp-H2-Titillium-Web">Medicamentos diarios</span>
       </div>
-      <Modal className="modal1" show={show} onHide={handleClose}>
+      <Modal
+        className="modal1"
+        show={show}
+        onHide={handleClose}
+        onEntered={handleModalEntered}
+        onScroll={handleModalScroll}
+        ref={modalRef}
+      >
         <Modal.Header closeButton>
-          <Modal.Title className="tituloModal2"> {'<  '} Hoy {'  >'}</Modal.Title>
+          
         </Modal.Header>
         <Modal.Body>
+          
           <Table className="medicamentos-table">
             <thead>
               <tr>
